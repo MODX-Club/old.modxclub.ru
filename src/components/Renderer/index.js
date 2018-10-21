7d0a5e47ed6c745b2049f09556246c1ffa20b52f
Fi1osof <n.lanets@modxclub.ru>
2018-10-22 02:21:32 +0300

    User router

--- src/components/Renderer/index.js
+++ src/components/Renderer/index.js
@@ -3,12 +3,35 @@ import React from "react";
 
 import { Renderer as PrismaCmsRenderer } from "@prisma-cms/front";
 
+import UserPage from "@prisma-cms/front/lib/modules/pages/UsersPage/UserPage";
 
 export default class BoilerplateRenderer extends PrismaCmsRenderer {
-  
+
   getRoutes() {
 
     let routers = super.getRoutes();
+
+    routers.unshift({
+      exact: true,
+      path: "/users/:userId",
+      render: (props) => {
+        const {
+          params,
+        } = props.match;
+
+        const {
+          userId,
+        } = params || {};
+
+        return <UserPage
+          key={userId}
+          where={{
+            id: parseInt(userId),
+          }}
+          {...props}
+        />
+      }
+    });
 
     return routers;
   }
